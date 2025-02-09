cc2625d51e8e00718bdaf77dbdb9ee4da666eb56
theflyfoxX <rassasali01@gmail.com>
2025-02-09 17:24:06 +0100

    Django project update 02/9

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models.like_model import Like
+from .models.post_model import Post
 
-# Register your models here.
+# Register the model
+admin.site.register(Like)
+admin.site.register(Post)
